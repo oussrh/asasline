5813bf9054cb8318ace7673a31c76ff9f5eaa952
MahmoudAbdo90 <mahmoud.mostafa25@gmail.com>
2021-03-10 21:12:13 +0100

    Adding quey && allPorts to my_functions

---
+++ my_functions/allPorts.js
@@ -0,0 +1,28 @@
+const query = require("./query")
+
+const GET_ALL_PORTS = `
+query {
+    allPorts {
+      data {
+        _id
+        description
+      }
+    }
+  }
+`
+
+exports.handler = async () => {
+  const { data, errors } = await query(GET_ALL_PORTS)
+
+  if (errors) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(errors),
+    }
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ messages: data.allPorts.data }),
+  }
+}
