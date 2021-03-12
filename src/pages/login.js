8f1925c3d513c5f1892868b13a99aec6e96575a5
MahmoudAbdo90 <mahmoud.mostafa25@gmail.com>
2021-03-12 21:30:50 +0100

    login component

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,21 +1,24 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import IdentityModal, {
   useIdentityContext,
 } from "react-netlify-identity-widget"
 import "react-netlify-identity-widget/styles.css"
-
+import Layout from "../components/layout"
 import "bootstrap/dist/css/bootstrap.min.css"
 import Button from "react-bootstrap/Button"
-import Modal from "react-bootstrap/Modal"
+import { navigate } from "gatsby"
+// import Modal from "react-bootstrap/Modal"
+const axios = require("axios")
 
 const Login = () => {
   const identity = useIdentityContext()
   const [dialog, setDialog] = useState(false)
+
   const email =
     (identity &&
       identity.user &&
       identity.user.user_metadata &&
-      identity.user.user_metadata.email) ||
+      identity.user.email) ||
     "Untitled"
   const name =
     (identity &&
@@ -25,27 +28,35 @@ const Login = () => {
     "Untitled"
   const isLoggedIn = identity && identity.isLoggedIn
 
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate("/profile")
+    }
+  })
+  useEffect(() => {
+    handleCreateUser(name, email)
+  }, [name, email, isLoggedIn])
+
   const handleCreateUser = async (name, email) => {
     await axios.post("/api/createUser", { name, email })
   }
 
   return (
+    // <Layout>
     <div>
       <IdentityModal
         showDialog={dialog}
         onCloseDialog={() => setDialog(false)}
       />
-
       {identity && identity.isLoggedIn ? (
         <div className="auth-btn-grp">
-          <p>User is In </p>{" "}
           <Button
             variant="outline-primary"
             className="login-btn"
             onClick={() => setDialog(true)}
           >
             {isLoggedIn
-              ? `Hello ${name}, Log out here!` && handleCreateUser(name, email)
+              ? `Hello ${name} first ${email}, Log out here!`
               : "LOG IN"}
           </Button>
         </div>
@@ -56,11 +67,12 @@ const Login = () => {
             className="login-btn"
             onClick={() => setDialog(true)}
           >
-            {isLoggedIn ? `Hello ${name}, Log out here!` : "LOG IN"}
+            {isLoggedIn ? `Hello ${name} Log out here!` : "LOG IN"}
           </Button>
         </div>
       )}
     </div>
+    // </Layout>
   )
 }
 
